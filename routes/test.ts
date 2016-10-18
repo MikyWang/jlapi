3d3292c86f4057970d7926397cf206db2f7492b8
Miky <813853090@qq.com>
2016-10-19 00:17:20 +0800

    初始化
    Signed-off-by: Miky <813853090@qq.com>

---
+++ routes/test.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+let router = express.Router();
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
